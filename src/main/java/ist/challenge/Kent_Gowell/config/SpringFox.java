5c2f2f62a2357351343cb31f2cbb04bb7311cb8e
KentGowell <gowellkent1@gmail.com>
2023-10-21 09:55:00 +0700

    rename SpringFox Class

--- src/main/java/ist/challenge/Kent_Gowell/config/SpringFox.java
+++ src/main/java/ist/challenge/Kent_Gowell/config/SpringFox.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
-public class SpringFoxTest extends WebMvcConfigurerAdapter {
+public class SpringFox extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
